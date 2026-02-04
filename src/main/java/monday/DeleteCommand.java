8e13f5b7faaf3c11c7dcef1b952470011bc9bdf9
PeterShanxin <shanxin@u.nus.edu>
2026-02-04 14:53:42 +0800

    Add task manipulation commands
    Let's add commands for task manipulation:
    - MarkCommand: Marks/unmarks tasks (single command with boolean flag)
    - DeleteCommand: Deletes tasks from the list
    - AddCommand: Abstract base for add commands
    - AddToDoCommand: Adds simple todo tasks

---
+++ src/main/java/monday/DeleteCommand.java
@@ -0,0 +1,64 @@
+package monday;
+
+/**
+ * Command to delete a task from the task list.
+ * Removes the specified task and updates the task count.
+ */
+public class DeleteCommand extends Command {
+
+    private final int taskNumber;
+
+    /**
+     * Creates a new delete command.
+     *
+     * @param taskNumber The 1-indexed task number to delete.
+     */
+    public DeleteCommand(int taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    /**
+     * Executes the delete command.
+     * Deletes the specified task and saves.
+     *
+     * @param taskList The task list to modify.
+     * @param ui The UI for displaying messages.
+     * @param storage The storage for persisting changes.
+     * @return A command result indicating save is needed, no exit.
+     * @throws CommandException If the task number is invalid.
+     */
+    @Override
+    public CommandResult execute(TaskList taskList, Ui ui, Storage storage) throws CommandException {
+        if (!taskList.isValidTaskNumber(taskNumber)) {
+            throw new CommandException(getInvalidTaskErrorMessage(taskList));
+        }
+
+        Task deletedTask = taskList.deleteTask(taskNumber);
+        ui.showTaskDeleted(deletedTask, taskList.getTaskCount());
+        return new CommandResult(true, false);
+    }
+
+    /**
+     * Checks if this command should exit the application.
+     *
+     * @return false, as this is not an exit command.
+     */
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+
+    /**
+     * Returns an error message for invalid task numbers.
+     *
+     * @param taskList The task list to check for size.
+     * @return The error message.
+     */
+    private String getInvalidTaskErrorMessage(TaskList taskList) {
+        if (taskList.isEmpty()) {
+            return "Skeptical. You haven't told me to do anything yet.";
+        } else {
+            return "Ugh, that task doesn't exist. Pick between 1 and " + taskList.getTaskCount() + ".";
+        }
+    }
+}
